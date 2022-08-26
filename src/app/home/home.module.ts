9b14460122052ca46ffe45f45065d15e1ef81341
SteelTitansax <manuel.portero.leiva@gmail.com>
2022-08-26 19:31:08 +0200

    First Commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { NgCircleProgressModule } from 'ng-circle-progress'; 
+import { HomePageRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth:16,
+      innerStrokeWidth:8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor:"#C7E596",
+      animationDuration:300,
+      animation: false,
+      responsive: true, 
+      renderOnClick: false
+    })
+  ],
+  declarations: [HomePage]
+})
+
+export class HomePageModule {}
